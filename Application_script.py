307fec2854608e128929fcd95a8ed8739d4cd140
Infantry-01 <111200119+Infantry-01@users.noreply.github.com>
2022-08-13 13:56:01 -0600

    Create Application_script.py

---
+++ Application_script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+space=' '*22
+print(''' To
+        The principal,
+        My School Name,
+        your city name.
+Dear Sir,
+        Sir, I am not feeling well, thus unable to join your class today
+Thanks
+Your sincerely
+Student NAme ''')
